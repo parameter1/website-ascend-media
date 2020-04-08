6e2b57e81d9b932b5d3c1eb4db79bd4fdaaaa087
Josh Worden <solocommand@gmail.com>
2020-04-08 16:08:29 -0500

    Add qcor & vascular navs

--- sites/hearthubs.org/config/navigation.js
+++ sites/hearthubs.org/config/navigation.js
@@ -1,12 +1,88 @@
 module.exports = {
+  /**
+   * QCOR
+   */
+  qcor: {
+    secondary: {
+      items: [
+        { href: '/qcor/science-news', label: 'Science News' },
+        { href: '/qcor/2020-program', label: 'QCOR 2020 Program' },
+        { href: '/qcor/learning-studios', label: 'Learning Studios' },
+      ],
+    },
+    tertiary: {
+      items: [
+        { href: '/search', label: 'Search', icon: 'search' },
+      ],
+    },
+    menu: [
+      {
+        label: 'Topics',
+        items: [
+          { href: '/qcor/science-news', label: 'Science News' },
+          { href: '/qcor/2020-program', label: 'QCOR 2020 Program' },
+          { href: '/qcor/learning-studios', label: 'Learning Studios' },
+        ],
+      },
+      {
+        label: 'User Tools',
+        items: [
+          { href: '/page/about-us', label: 'About Us' },
+          { href: '/contact-us', label: 'Contact Us' },
+          { href: '/page/privacy-policy', label: 'Privacy Policy' },
+          { href: '/page/terms-conditions', label: 'Terms & Conditions' },
+        ],
+      },
+    ],
+  },
+  /**
+   * Vascular
+   */
+  vascular: {
+    secondary: {
+      items: [
+        { href: '/vascular/science-news', label: 'Science News' },
+        { href: '/vascular/2020-program', label: 'Vascular Discovery 2020 Program' },
+        { href: '/vascular/learning-studios', label: 'Learning Studios' },
+      ],
+    },
+    tertiary: {
+      items: [
+        { href: '/search', label: 'Search', icon: 'search' },
+      ],
+    },
+    menu: [
+      {
+        label: 'Topics',
+        items: [
+          { href: '/vascular/science-news', label: 'Science News' },
+          { href: '/vascular/2020-program', label: 'Vascular Discovery 2020 Program' },
+          { href: '/vascular/learning-studios', label: 'Learning Studios' },
+        ],
+      },
+      {
+        label: 'User Tools',
+        items: [
+          { href: '/page/about-us', label: 'About Us' },
+          { href: '/contact-us', label: 'Contact Us' },
+          { href: '/page/privacy-policy', label: 'Privacy Policy' },
+          { href: '/page/terms-conditions', label: 'Terms & Conditions' },
+        ],
+      },
+    ],
+  },
+  /**
+   * Site defaults -- when not actively under a conference
+   */
   primary: {
     items: [
-      { href: '/qcor', label: 'QCOR' },
-      { href: '/vascular', label: 'Vascular' },
     ],
   },
   secondary: {
+    // List available conferences
     items: [
+      { href: '/qcor', label: 'QCOR 2020 Conference' },
+      { href: '/vascular', label: 'Vascular Discovery 2020 Conference' },
     ],
   },
   tertiary: {
