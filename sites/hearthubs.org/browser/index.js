ed709b518b15f46de4195f4a8573d1a41ea8f859
Josh Worden <solocommand@gmail.com>
2020-04-08 16:08:13 -0500

    create browser components

--- sites/hearthubs.org/browser/index.js
+++ sites/hearthubs.org/browser/index.js
@@ -1,6 +1,16 @@
 import Browser from '@base-cms/marko-web/browser';
 import Shared from '@ascend-media/package-shared/browser';
 
+const CompanyProfileWebsiteLink = () => import(/* webpackChunkName: "company-profile-website-link" */ './website-link.vue');
+const CompanyProfileSocialLink = () => import(/* webpackChunkName: "company-profile-social-link" */ './social-link.vue');
+
 Shared(Browser);
+const { EventBus } = Browser;
+Browser.register('CompanyProfileWebsiteLink', CompanyProfileWebsiteLink, {
+  on: { action: (...args) => EventBus.$emit('leaders-action', ...args) },
+});
+Browser.register('CompanyProfileSocialLink', CompanyProfileSocialLink, {
+  on: { action: (...args) => EventBus.$emit('leaders-action', ...args) },
+});
 
 export default Browser;
