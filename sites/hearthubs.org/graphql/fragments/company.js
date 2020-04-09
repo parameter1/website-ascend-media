7aadec63f8f7022a882a5bc7c8543e0333c15ce2
Josh Worden <solocommand@gmail.com>
2020-04-09 11:11:57 -0500

    Customize company page

---
+++ sites/hearthubs.org/graphql/fragments/company.js
@@ -0,0 +1,102 @@
+const gql = require('graphql-tag');
+
+module.exports = gql`
+
+fragment WebsiteContentCompanyFragment on Content {
+  id
+  type
+  name
+  teaser(input: { maxLength: 0 })
+  body
+
+  primarySection {
+    id
+    name
+    alias
+    logo {
+      src(input: { options: { h: 100 } })
+    }
+  }
+
+  siteContext {
+    path
+  }
+  ... on ContentCompany {
+
+    address1
+    address2
+    cityStateZip
+    country
+
+    phone
+    tollfree
+    fax
+    website
+    title
+    mobile
+    email
+    publicEmail
+
+    youtube {
+      username
+      channelId
+      url
+    }
+
+    # kv data
+    yearsInOperation
+    numberOfEmployees
+    salesChannels
+    salesRegion
+
+    # long text data
+    productSummary
+    servicesProvided
+    serviceInformation
+    trainingInformation
+    warrantyInformation
+
+    # circle image
+    primaryImage {
+      id
+      src
+      alt
+    }
+
+    contacts: publicContacts {
+      edges {
+        node {
+          id
+          name
+          title
+          publicEmail
+          primaryImage {
+            id
+            src(input: { options: { auto: "format", h: 100, w: 100, mask: "ellipse", fit: "facearea", facepad: 3 } })
+          }
+        }
+      }
+    }
+
+    videos: youtubeVideos(input: { pagination: { limit: 3 } }) {
+      edges {
+        node {
+          id
+          url
+          title
+          published
+          thumbnail(input: { size: high })
+        }
+      }
+    }
+  }
+  ... on SocialLinkable {
+    socialLinks {
+      provider
+      url
+      label
+    }
+  }
+}
+
+`;
--- sites/hearthubs.org/server/routes/content.js
+++ sites/hearthubs.org/server/routes/content.js
@@ -1,8 +1,14 @@
 const { withContent } = require('@base-cms/marko-web/middleware');
 const content = require('@ascend-media/package-shared/templates/content');
 const queryFragment = require('@ascend-media/package-shared/graphql/fragments/content-page');
+const companyQueryFragment = require('../../graphql/fragments/company');
+const company = require('../templates/content/company');
 
 module.exports = (app) => {
+  app.get('/*?company/:id(\\d{8})*', withContent({
+    template: company,
+    queryFragment: companyQueryFragment,
+  }));
   app.get('/*?:id(\\d{8})*', withContent({
     template: content,
     queryFragment,
