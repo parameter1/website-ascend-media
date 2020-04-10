6db48c231a754c3ae5a4ebc93e8ffae6e6fd5660
Josh Worden <solocommand@gmail.com>
2020-04-10 11:53:03 -0500

    remove reveal references

--- packages/shared/browser/index.js
+++ packages/shared/browser/index.js
@@ -2,7 +2,6 @@ import DefaultTheme from '@base-cms/marko-web-theme-default/browser';
 import GTM from '@base-cms/marko-web-gtm/browser';
 import GAM from '@base-cms/marko-web-gam/browser';
 import GCSE from '@base-cms/marko-web-gcse/browser';
-import RevealAd from '@base-cms/marko-web-reveal-ad/browser';
 import SocialSharing from '@base-cms/marko-web-social-sharing/browser';
 import PhotoSwipe from '@base-cms/marko-web-photoswipe/browser';
 
@@ -13,7 +12,6 @@ export default (Browser) => {
   GTM(Browser);
   GAM(Browser);
   GCSE(Browser);
-  RevealAd(Browser);
   SocialSharing(Browser);
   PhotoSwipe(Browser);
   ContactUs(Browser);
