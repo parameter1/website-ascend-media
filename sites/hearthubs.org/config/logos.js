df8cf8c61c02a4eaf47dc983fb298308bc9be22e
Jacob Bare <jacob@limit0.io>
2020-04-16 14:35:20 -0500

    Add contextual logo href and alt/title

--- sites/hearthubs.org/config/logos.js
+++ sites/hearthubs.org/config/logos.js
@@ -4,6 +4,7 @@ module.exports = {
     srcset: [
       'https://base.imgix.net/files/base/ascend/hearthub/image/static/footer.svg?h=180 2x',
     ],
+    href: '/',
     contexts: [
       {
         when: ['/vascular'],
@@ -11,6 +12,8 @@ module.exports = {
         srcset: [
           'https://base.imgix.net/files/base/ascend/hearthub/image/static/vascular-discovery.png?h=180 2x',
         ],
+        href: '/vascular',
+        title: 'Vascular Discovery: From Genes to Medicine',
       },
       {
         when: ['/qcor'],
@@ -18,6 +21,8 @@ module.exports = {
         srcset: [
           'https://base.imgix.net/files/base/ascend/hearthub/image/static/qcor.png?h=180 2x',
         ],
+        href: '/qcor',
+        title: 'Quality of Care & Outcomes Reasearch (QCOR)',
       },
     ],
   },
