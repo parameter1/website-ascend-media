e1ffc5eea0979fd1de50c69a9794bfcf3332424e
Jacob Bare <jacob@limit0.io>
2020-04-16 22:12:26 -0500

    Move vascular to own template

--- sites/hearthubs.org/server/routes/website-section.js
+++ sites/hearthubs.org/server/routes/website-section.js
@@ -2,7 +2,14 @@ const { withWebsiteSection } = require('@base-cms/marko-web/middleware');
 const queryFragment = require('../graphql/fragments/website-section-page');
 const section = require('../templates/website-section');
 
+const vascular = require('../templates/website-section/vascular/index');
+
 module.exports = (app) => {
+  app.get('/:alias(vascular)', withWebsiteSection({
+    template: vascular,
+    queryFragment,
+  }));
+
   app.get('/:alias([a-z0-9-/]+)', withWebsiteSection({
     template: section,
     queryFragment,
