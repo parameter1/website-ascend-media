2d536c79c78e476bb5689921728ec8941cd2e8e4
Jacob Bare <jacob@limit0.io>
2020-04-16 21:04:07 -0500

    Create featured exhibitors block

---
+++ sites/hearthubs.org/server/graphql/fragments/featured-exhibitors.js
@@ -0,0 +1,16 @@
+const gql = require('graphql-tag');
+
+module.exports = gql`
+
+fragment WebsiteContentFeaturedExhibitorsFragment on Content {
+  id
+  type
+  primaryImage {
+    id
+    src
+    alt
+    isLogo
+  }
+}
+
+`;
