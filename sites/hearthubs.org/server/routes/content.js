3f57c22eb601dde0d186dcf217c810ead9fab7d6
Jacob Bare <jacob@limit0.io>
2020-04-17 07:48:43 -0500

    Additional content page rework

--- sites/hearthubs.org/server/routes/content.js
+++ sites/hearthubs.org/server/routes/content.js
@@ -1,13 +1,13 @@
 const { withContent } = require('@base-cms/marko-web/middleware');
-const queryFragment = require('@ascend-media/package-shared/graphql/fragments/content-page');
-const companyQueryFragment = require('../../graphql/fragments/company');
+const queryFragment = require('../graphql/fragments/content-page');
+const companyFragment = require('../graphql/fragments/company-page');
 const content = require('../templates/content');
 const company = require('../templates/content/company');
 
 module.exports = (app) => {
   app.get('/*?company/:id(\\d{8})*', withContent({
     template: company,
-    queryFragment: companyQueryFragment,
+    queryFragment: companyFragment,
   }));
   app.get('/*?:id(\\d{8})*', withContent({
     template: content,
