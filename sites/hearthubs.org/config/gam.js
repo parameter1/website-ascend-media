34c9482b1c415bd2e388d2c05de4c94bf2c9a35f
Jacob Bare <jacob@limit0.io>
2020-04-27 10:59:45 -0500

    Add 970x250 to LB template

--- sites/hearthubs.org/config/gam.js
+++ sites/hearthubs.org/config/gam.js
@@ -4,9 +4,9 @@ const config = new GAMConfiguration('6407152', { basePath: 'hearthubs' });
 
 config
   .setTemplate('LB', {
-    size: [[970, 90], [728, 90], [300, 50]],
+    size: [[970, 250], [970, 90], [728, 90], [300, 50]],
     sizeMapping: [
-      { viewport: [980, 0], size: [[970, 90], [728, 90]] },
+      { viewport: [980, 0], size: [[970, 250], [970, 90], [728, 90]] },
       { viewport: [750, 0], size: [728, 90] },
       { viewport: [320, 0], size: [300, 50] },
     ],
