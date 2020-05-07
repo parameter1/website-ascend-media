50dc369909ba5b894daa4122d7ca445fdbefd5d4
Brandon Krigbaum <brandonbk@gmail.com>
2020-05-07 09:06:53 -0500

    Add qcor routes

--- sites/hearthubs.org/server/routes/website-section/index.js
+++ sites/hearthubs.org/server/routes/website-section/index.js
@@ -2,9 +2,12 @@ const { withWebsiteSection } = require('@base-cms/marko-web/middleware');
 const queryFragment = require('../../graphql/fragments/website-section-page');
 const section = require('../../templates/website-section');
 const vascular = require('./vascular');
+const qcor = require('./qcor');
 
 module.exports = (app) => {
   vascular(app);
+
+  qcor(app);
 
   app.get('/:alias([a-z0-9-/]+)', withWebsiteSection({
     template: section,
