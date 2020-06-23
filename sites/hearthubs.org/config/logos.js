2daa8dc9b78572090963f9646200144e3aa0c600
Brandon Krigbaum <brandonbk@gmail.com>
2020-06-23 16:59:24 -0500

    Update logos

--- sites/hearthubs.org/config/logos.js
+++ sites/hearthubs.org/config/logos.js
@@ -25,6 +25,24 @@ module.exports = {
         href: '/qcor',
         title: 'Quality of Care & Outcomes Reasearch (QCOR)',
       },
+      {
+        when: ['/bcvs'],
+        src: 'https://p1-cms-assets-ascend.imgix.net/files/base/ascend/hearthub/image/static/bcvs-logo.png?h=90',
+        srcset: [
+          'https://p1-cms-assets-ascend.imgix.net/files/base/ascend/hearthub/image/static/bcvs-logo.png?h=180 2x',
+        ],
+        href: '/bcvs',
+        title: 'Basic Cardiovascular Sciences (BCVS)',
+      },
+      {
+        when: ['/hypertension'],
+        src: 'https://p1-cms-assets-ascend.imgix.net/files/base/ascend/hearthub/image/static/hypertension-logo.png?h=90',
+        srcset: [
+          'https://p1-cms-assets-ascend.imgix.net/files/base/ascend/hearthub/image/static/hypertension-logo.png?h=180 2x',
+        ],
+        href: '/hypertension',
+        title: 'Hypertension',
+      },
     ],
   },
   footer: {
