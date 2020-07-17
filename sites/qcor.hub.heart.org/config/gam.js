253e774c045e06e23d8bbdb2b536b1769fdb695d
Brandon Krigbaum <brandonbk@gmail.com>
2020-07-16 22:43:19 -0500

    Update gam config

--- sites/qcor.hub.heart.org/config/gam.js
+++ sites/qcor.hub.heart.org/config/gam.js
@@ -1,28 +1,12 @@
-const GAMConfiguration = require('@base-cms/marko-web-gam/config');
+const configureGAM = require('@ascend-media/package-shared/config/gam');
 
-const config = new GAMConfiguration('6407152', { basePath: 'hearthubs' });
-
-config
-  .setTemplate('LB', {
-    size: [[970, 250], [970, 90], [728, 90], [300, 50]],
-    sizeMapping: [
-      { viewport: [980, 0], size: [[970, 250], [970, 90], [728, 90]] },
-      { viewport: [750, 0], size: [728, 90] },
-      { viewport: [320, 0], size: [300, 50] },
-    ],
-  })
-  .setTemplate('RAIL1', { size: [[300, 250]] });
+const config = configureGAM({ basePath: 'hearthubs' });
 
 config
   .setAliasAdUnits('default', [
     { name: 'lb1', templateName: 'LB', path: 'lb1' },
     { name: 'rail1', templateName: 'RAIL1', path: 'rail1' },
     { name: 'rail2', templateName: 'RAIL1', path: 'rail2' },
-  ])
-  .setAliasAdUnits('vascular', [
-    { name: 'lb1', templateName: 'LB', path: 'vascular-lb1' },
-    { name: 'rail1', templateName: 'RAIL1', path: 'vascular-rail1' },
-    { name: 'rail2', templateName: 'RAIL1', path: 'vascular-rail2' },
   ]);
 
 module.exports = config;
