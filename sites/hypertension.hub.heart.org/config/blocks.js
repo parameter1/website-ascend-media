c1275b6959e1f1db9cd0e0c476f8ed6313110101
Brandon Krigbaum <brandonbk@gmail.com>
2020-07-16 22:47:23 -0500

    Add hypertension

---
+++ sites/hypertension.hub.heart.org/config/blocks.js
@@ -0,0 +1,27 @@
+module.exports = {
+  leaderboard: {
+    src: 'https://p1-cms-assets-ascend.imgix.net/files/base/ascend/hearthub/image/static/hypertension-billboard.png',
+    href: '/',
+  },
+  exhibits: {
+    src: 'https://p1-cms-assets-ascend.imgix.net/files/base/ascend/hearthub/image/static/hypertension-exhibits.png',
+    href: 'https://www.hearthubs.org/hypertension/exhibitors',
+  },
+  eposters: {
+    src: 'https://p1-cms-assets-ascend.imgix.net/files/base/ascend/hearthub/image/static/hypertension-eposters.png',
+    href: '/',
+  },
+  'early-career': {
+    sectionAlias: 'early-career',
+    href: 'https://earlycareervoice.professional.heart.org/blog',
+  },
+  'watch-listen': {
+    sectionAlias: 'watch-listen',
+  },
+  'access-sessions': {
+    href: 'https://professional.heart.org/professional/EducationMeetings/MeetingsLiveCME/ATVBPVD/UCM_316902_Vascular-Discovery-From-Genes-to-Medicine.jsp',
+  },
+  'science-news': {
+    href: 'https://professional.heart.org/professional/EducationMeetings/MeetingsLiveCME/ATVBPVD/UCM_506015_Science-News-for-Vascular-Discovery-2020.jsp',
+  },
+};
