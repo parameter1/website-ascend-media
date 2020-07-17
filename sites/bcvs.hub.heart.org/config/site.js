b7e0e9579a8670bf6e5de3d8d97d9e273cf20290
Brandon Krigbaum <brandonbk@gmail.com>
2020-07-17 10:09:29 -0500

    Update GTM container id

--- sites/bcvs.hub.heart.org/config/site.js
+++ sites/bcvs.hub.heart.org/config/site.js
@@ -18,7 +18,7 @@ module.exports = {
     { provider: 'linkedin', href: 'https://www.linkedin.com/company/american-heart-association', target: '_blank' },
   ],
   gtm: {
-    containerId: 'GTM-5RV2BC2',
+    containerId: 'GTM-KTZXFNP',
   },
   gcse: {
     id: '003355913687346718228:arteokcijaj',
