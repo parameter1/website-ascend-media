fee6429d3f34b0fd7989eb8cf899f10c251d6366
Brandon Krigbaum <brandonbk@gmail.com>
2020-08-25 10:12:02 -0500

    Update navigation

--- sites/minexpo.com/config/navigation.js
+++ sites/minexpo.com/config/navigation.js
@@ -1,13 +1,6 @@
 module.exports = {
   secondary: {
     items: [
-      { href: 'https://pmmi.dragonforms.com/loading.do?pk=PWMagNav&oly_enc_id=@{encrypted_customer_id}@&omedasite=PPWnew', label: 'Subscribe', target: '_blank' },
-      { href: '/machinery', label: 'Machinery' },
-      { href: '/design', label: 'Design' },
-      { href: '/issues', label: 'Issues' },
-      { href: '/page/pw-event-calendar', label: 'Events' },
-      { href: '/downloads', label: 'Downloads' },
-      { href: 'https://cobotspot.packworld.com', label: 'Collaborative Robots', target: '_blank' },
     ],
   },
   tertiary: {
@@ -17,52 +10,28 @@ module.exports = {
   },
   footer: {
     items: [
-      { href: 'https://www.pmmimediagroup.com/pw/packaging-world', label: 'Advertise', target: '_blank' },
-      { href: '/page/pw-about-us', label: 'About' },
-      { href: '/page/pw-contact-us', label: 'Contact' },
-      { href: 'https://www.pmmimediagroup.com/privacy', label: 'Privacy', target: '_blank' },
-      { href: '/page/pw-terms-use', label: 'Terms of Use' },
-      { href: 'https://www.pmmimediagroup.com/advertising-terms-conditions', label: 'Advertising Terms & Conditions', target: '_blank' },
+      // { href: 'https://www.pmmimediagroup.com/pw/packaging-world', label: 'Advertise', target: '_blank' },
+      // { href: '/page/pw-about-us', label: 'About' },
+      // { href: '/page/pw-contact-us', label: 'Contact' },
+      // { href: 'https://www.pmmimediagroup.com/privacy', label: 'Privacy', target: '_blank' },
+      // { href: '/page/pw-terms-use', label: 'Terms of Use' },
+      // { href: 'https://www.pmmimediagroup.com/advertising-terms-conditions', label: 'Advertising Terms & Conditions', target: '_blank' },
     ],
   },
   menu: [
     {
       label: 'Topics',
       items: [
-        { href: '/machinery', label: 'Machinery' },
-        { href: '/design', label: 'Design' },
-        { href: '/issues', label: 'Issues' },
       ],
     },
     {
       label: 'Resources',
       items: [
-        // { href: '/magazine', label: 'Magazine' },
-        { href: '/page/contract-packaging-magazine', label: 'Contract Packaging' },
-        { href: '/page/pw-event-calendar', label: 'Events' },
-        { href: '/downloads', label: 'Downloads' },
-        { href: '/page/pw-newsletters', label: 'Newsletters' },
-        { href: '/page/magazine', label: 'Magazine' },
-        { href: '/leaders', label: 'Leaders in Packaging' },
-        { href: '/videos', label: 'Videos' },
-        { href: '/page/packaging-associations', label: 'Packaging Associations' },
-        { href: 'https://www.pmmi.org/hall-of-fame', label: 'Hall of Fame', target: '_blank' },
-        { href: '/webinars', label: 'Webinars' },
-        { href: 'https://cobotspot.packworld.com', label: 'Collaborative Robots', target: '_blank' },
-        // { href: '/page/digital-editions', label: 'Digital Editions' },
       ],
     },
     {
       label: 'User Tools',
       items: [
-        { href: 'https://pmmi.dragonforms.com/loading.do?pk=PWMagNav&oly_enc_id=@{encrypted_customer_id}@&omedasite=PPWnew', label: 'Subscribe', target: '_blank' },
-        { href: 'https://www.pmmimediagroup.com/pw/packaging-world', label: 'Advertise', target: '_blank' },
-        { href: 'https://www.pmmimediagroup.com/pw/reprints-and-permissions', label: 'Reprints' },
-        { href: '/page/pw-about-us', label: 'About' },
-        { href: '/page/pw-contact-us', label: 'Contact' },
-        { href: 'https://www.pmmimediagroup.com/privacy', label: 'Privacy', target: '_blank' },
-        { href: '/page/pw-terms-use', label: 'Terms of Use' },
-        { href: 'https://www.pmmimediagroup.com/advertising-terms-conditions', label: 'Advertising Terms & Conditions', target: '_blank' },
       ],
     },
   ],
