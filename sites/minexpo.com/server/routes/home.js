d24d79280733e8bf94534f5bb88df8f85daf7fc8
Brandon Krigbaum <brandonbk@gmail.com>
2020-08-25 10:11:35 -0500

    Temporarily set home page template to leaders

--- sites/minexpo.com/server/routes/home.js
+++ sites/minexpo.com/server/routes/home.js
@@ -1,11 +1,12 @@
 const { withWebsiteSection } = require('@base-cms/marko-web/middleware');
-const home = require('../templates/index');
+// const home = require('../templates/index');
+const leaders = require('../templates/website-section/leaders');
 const queryFragment = require('../graphql/fragments/website-section-page');
 
 module.exports = (app) => {
   app.get('/', withWebsiteSection({
     aliasResolver: () => 'home',
-    template: home,
+    template: leaders,
     queryFragment,
   }));
 };
