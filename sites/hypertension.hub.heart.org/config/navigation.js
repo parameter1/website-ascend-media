1ff8c58855c7f2f0f8fba22eff61e19394f06c37
Brandon Krigbaum <brandonbk@gmail.com>
2020-09-03 13:44:17 -0500

    Update journals link

--- sites/hypertension.hub.heart.org/config/navigation.js
+++ sites/hypertension.hub.heart.org/config/navigation.js
@@ -14,7 +14,7 @@ const topics = [
   { href: '/hypertension/2020-program', label: '2020 Schedule' },
   { href: '/exhibitors', label: 'Exhibits' },
   { href: 'https://professional.heart.org/en/meetings/hypertension/science-news-from-hypertension-2020', label: 'Science News', target: '_blank' },
-  { href: 'https://www.ahajournals.org', label: 'Journals', target: '_blank' },
+  { href: 'https://www.ahajournals.org/hypertension-sessions', label: 'Journals', target: '_blank' },
   { href: '/hypertension/membership', label: 'Membership' },
   { href: 'https://learn.heart.org', label: 'Claim CME', target: '_blank' },
 ];
