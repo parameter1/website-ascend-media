591795d79afb8866059fc7a46693b8c88064218d
Brandon Krigbaum <brandonbk@gmail.com>
2020-09-21 21:07:35 -0500

    Initial commit of imex

---
+++ sites/imex.ascendmedia.com/config/navigation.js
@@ -0,0 +1,46 @@
+const searchItem = {
+  href: '/search',
+  label: 'Search',
+  icon: 'search',
+  modifiers: ['search-icon'],
+};
+
+const userTools = [
+  { href: '/page/privacy-policy', label: 'Privacy Policy' },
+  { href: '/page/terms-conditions', label: 'Terms & Conditions' },
+];
+
+const topics = [
+  { href: 'https://learn.heart.org', label: 'Claim CME', target: '_blank' },
+];
+
+const secondary = [
+  { href: 'https://htn.apprisor.org', label: 'ePosters Access', target: '_blank' },
+];
+
+module.exports = {
+
+  primary: {
+    items: [...topics],
+  },
+  secondary: {
+    items: [...secondary],
+  },
+  tertiary: {
+    items: [searchItem],
+  },
+  menu: [
+    {
+      label: 'Topics',
+      items: [...topics],
+    },
+    {
+      label: 'Resources',
+      items: [...secondary],
+    },
+    {
+      label: 'User Tools',
+      items: [...userTools],
+    },
+  ],
+};
