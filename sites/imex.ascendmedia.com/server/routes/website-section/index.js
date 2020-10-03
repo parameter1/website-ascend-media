b1324435647e7cc48a1b70ff83475a1a6c15d2e3
Brandon Krigbaum <brandonbk@gmail.com>
2020-10-03 16:15:48 -0500

    Drop unused routes

--- sites/imex.ascendmedia.com/server/routes/website-section/index.js
+++ sites/imex.ascendmedia.com/server/routes/website-section/index.js
@@ -1,23 +1,11 @@
 const { withWebsiteSection } = require('@base-cms/marko-web/middleware');
-const membership = require('@ascend-media/package-shared/templates/website-section/membership');
-const program2020 = require('../../templates/website-section/2020-program');
 const queryFragment = require('@ascend-media/package-daily/graphql/fragments/website-section-page');
 const section = require('@ascend-media/package-daily/templates/website-section');
 const exhibitors = require('@ascend-media/package-daily/templates/website-section/exhibitors');
 
 module.exports = (app) => {
-  app.get('/:alias(2020-program)', withWebsiteSection({
-    template: program2020,
-    queryFragment,
-  }));
-
   app.get('/:alias(exhibitors)', withWebsiteSection({
     template: exhibitors,
-    queryFragment,
-  }));
-
-  app.get('/:alias(membership)', withWebsiteSection({
-    template: membership,
     queryFragment,
   }));
 
