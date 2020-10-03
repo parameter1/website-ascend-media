05929e226dc71a76a58a9368412d541f75fa4749
Brandon Krigbaum <brandonbk@gmail.com>
2020-10-03 16:14:21 -0500

    Add column1 ad unit

--- sites/imex.ascendmedia.com/config/gam.js
+++ sites/imex.ascendmedia.com/config/gam.js
@@ -7,6 +7,7 @@ config
     { name: 'lb1', templateName: 'LB', path: 'lb1' },
     { name: 'rail1', templateName: 'RAIL1', path: 'rail1' },
     { name: 'rail2', templateName: 'RAIL1', path: 'rail2' },
+    { name: 'column1', templateName: 'COLUMN1', path: 'column1' },
   ]);
 
 module.exports = config;
