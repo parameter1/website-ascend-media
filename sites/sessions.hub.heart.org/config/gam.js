d24c264a6a8ddf56ba611d1f264f3a1b6cd243bf
Brandon Krigbaum <brandonbk@gmail.com>
2020-10-11 12:18:08 -0500

    Add rail2 ad unit

--- sites/sessions.hub.heart.org/config/gam.js
+++ sites/sessions.hub.heart.org/config/gam.js
@@ -7,6 +7,7 @@ config
     { name: 'lb1', templateName: 'LB', path: 'lb1' },
     { name: 'lb2', templateName: 'LB', path: 'lb2' },
     { name: 'rail1', templateName: 'RAIL1', path: 'rail1' },
+    { name: 'rail2', templateName: 'RAIL1', path: 'rail2' },
     { name: 'load-more', templateName: 'RAIL1', path: 'load-more' },
   ]);
 
