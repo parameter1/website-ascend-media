e13ef8348d6e912f3b693681405581de7945ddc4
Brandon Krigbaum <brandonbk@gmail.com>
2020-10-11 11:17:52 -0500

    Update GAM config

--- sites/sessions.hub.heart.org/config/gam.js
+++ sites/sessions.hub.heart.org/config/gam.js
@@ -1,12 +1,13 @@
 const configureGAM = require('@ascend-media/package-shared/config/gam');
 
-const config = configureGAM({ basePath: 'hearthubs' });
+const config = configureGAM({ basePath: 'sessions' });
 
 config
   .setAliasAdUnits('default', [
     { name: 'lb1', templateName: 'LB', path: 'lb1' },
+    { name: 'lb2', templateName: 'LB', path: 'lb2' },
     { name: 'rail1', templateName: 'RAIL1', path: 'rail1' },
-    { name: 'rail2', templateName: 'RAIL1', path: 'rail2' },
+    { name: 'load-more', templateName: 'RAIL1', path: 'load-more' },
   ]);
 
 module.exports = config;
