c5b86a879c0c71bf1f8ab3764a0a88662fd76024
Brandon Krigbaum <brandonbk@gmail.com>
2020-10-21 13:45:11 -0500

    Add compression to images

--- sites/sessions.hub.heart.org/config/blocks.js
+++ sites/sessions.hub.heart.org/config/blocks.js
@@ -1,14 +1,14 @@
 module.exports = {
   leaderboard: {
-    src: 'https://p1-cms-assets-ascend.imgix.net/files/base/ascend/hearthub/image/static/hypertension-billboard-2.png',
+    src: 'https://p1-cms-assets-ascend.imgix.net/files/base/ascend/hearthub/image/static/hypertension-billboard-2.png?auto=format,compress&q=70',
     href: '/',
   },
   exhibits: {
-    src: 'https://p1-cms-assets-ascend.imgix.net/files/base/ascend/hearthub/image/static/hypertension-exhibits.png',
+    src: 'https://p1-cms-assets-ascend.imgix.net/files/base/ascend/hearthub/image/static/hypertension-exhibits.png?auto=format,compress&q=70',
     href: '/',
   },
   eposters: {
-    src: 'https://p1-cms-assets-ascend.imgix.net/files/base/ascend/hearthub/image/static/hypertension-eposters.png',
+    src: 'https://p1-cms-assets-ascend.imgix.net/files/base/ascend/hearthub/image/static/hypertension-eposters.png?auto=format,compress&q=70',
     href: '/',
   },
   'early-career': {
@@ -25,7 +25,7 @@ module.exports = {
     href: '/',
   },
   'exhibitors-banner': {
-    src: 'https://p1-cms-assets-ascend.imgix.net/files/base/ascend/hearthub/image/static/hypertension-exhibitors-banner.png',
+    src: 'https://p1-cms-assets-ascend.imgix.net/files/base/ascend/hearthub/image/static/hypertension-exhibitors-banner.png?auto=format,compress&q=70',
     href: '/exhibitors',
   },
 };
