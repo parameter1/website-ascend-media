17252c8c6c60cde6a89197cf24b76e3e744243b6
Brandon Krigbaum <brandonbk@gmail.com>
2020-11-03 07:37:35 -0600

    Copy sessions to isc

---
+++ sites/isc.hub.heart.org/config/blocks.js
@@ -0,0 +1,31 @@
+module.exports = {
+  leaderboard: {
+    src: 'https://p1-cms-assets-ascend.imgix.net/files/base/ascend/hearthub/image/static/hypertension-billboard-2.png?auto=format,compress&q=70',
+    href: '/',
+  },
+  exhibits: {
+    src: 'https://p1-cms-assets-ascend.imgix.net/files/base/ascend/hearthub/image/static/hypertension-exhibits.png?auto=format,compress&q=70',
+    href: '/',
+  },
+  eposters: {
+    src: 'https://p1-cms-assets-ascend.imgix.net/files/base/ascend/hearthub/image/static/hypertension-eposters.png?auto=format,compress&q=70',
+    href: '/',
+  },
+  'early-career': {
+    sectionAlias: 'early-career',
+    href: '/',
+  },
+  'watch-listen': {
+    sectionAlias: 'watch-listen',
+  },
+  'access-sessions': {
+    href: '/',
+  },
+  'science-news': {
+    href: '/',
+  },
+  'exhibitors-banner': {
+    src: 'https://p1-cms-assets-ascend.imgix.net/files/base/ascend/hearthub/image/static/hypertension-exhibitors-banner.png?auto=format,compress&q=70',
+    href: '/exhibitors',
+  },
+};
---
+++ sites/isc.hub.heart.org/config/logos.js
@@ -0,0 +1,16 @@
+module.exports = {
+  navbar: {
+    src: 'https://p1-cms-assets-ascend.imgix.net/files/base/ascend/hearthub/image/static/sessions_logo.png?h=90&auto=format,compress&q=70',
+    srcset: [
+      'https://p1-cms-assets-ascend.imgix.net/files/base/ascend/hearthub/image/static/sessions_logo.png?h=180&auto=format,compress&q=70 2x',
+    ],
+    href: '/',
+    title: 'Scientific Sessions',
+  },
+  footer: {
+    src: 'https://p1-cms-assets-ascend.imgix.net/files/base/ascend/hearthub/image/static/footer.svg?h=90&auto=format,compress&q=70',
+    srcset: [
+      'https://p1-cms-assets-ascend.imgix.net/files/base/ascend/hearthub/image/static/footer.svg?h=180&auto=format,compress&q=70 2x',
+    ],
+  },
+};
