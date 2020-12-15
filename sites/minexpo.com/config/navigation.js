cf941889b7db334a8182d896d8077ee45e082a58
Brian Miller <brian@parameter1.com>
2020-12-15 13:50:29 -0600

    update navigation items

--- sites/minexpo.com/config/navigation.js
+++ sites/minexpo.com/config/navigation.js
@@ -1,18 +1,31 @@
 const topics = [
-  { href: '/directory', label: 'Directory' },
+  { href: '/directory/auxiliary-equipment-and-supplies', label: 'Auxiliary' },
+  { href: '/directory/components-and-replacement-equipment', label: 'Components' },
+  { href: '/directory/electrical-equipment-and-supplies', label: 'Electrical' },
+  { href: '/directory/engineering-construction-consulting-and-mining-related-services', label: 'Engineering/Consulting/Consulting' },
+  { href: 'material-handling-equipment', label: 'Material Handling' },
+  // { href: '/directory/mining-equipment', label: 'Minning' },
+  { href: '/directory/other-related-equipment-products-and-services', label: 'Other' },
+  { href: '/directory/power-and-power-transmission-equipment', label: 'Power' },
+  { href: '/directory/processingpreparation-equipment', label: 'Processing/Preperation' },
+];
+
+const resources = [
+  { href: 'https://ratecards.ascendeventmedia.com/minexpo2020/', label: 'Advertise', target: '_blank' },
+  { href: 'https://ratecards.ascendeventmedia.com/minexpo2020/', label: 'About', target: '_blank' },
+  { href: 'https://ratecards.ascendeventmedia.com/minexpo2020/#contact', label: 'Contact', target: '_blank' },
 ];
 
 module.exports = {
   primary: {
-    items: [
-    ],
+    items: [...topics],
   },
   secondary: {
-    items: [...topics],
+    items: [...resources],
   },
   tertiary: {
     items: [
-      { href: '/search', label: 'Search', icon: 'search' },
+      // { href: '/search', label: 'Search', icon: 'search' },
     ],
   },
   footer: {
@@ -28,12 +41,22 @@ module.exports = {
   menu: [
     {
       label: 'Topics',
-      items: [...topics],
+      items: [
+        // { href: '/directory', label: 'Full Directory' },
+        { href: '/directory/auxiliary-equipment-and-supplies', label: 'Auxiliary Equipment and Supplies' },
+        { href: '/directory/components-and-replacement-equipment', label: 'Components & Replacement Equipment' },
+        { href: '/directory/electrical-equipment-and-supplies', label: 'Electrical Equipment & Supplies' },
+        { href: '/directory/engineering-construction-consulting-and-mining-related-services', label: 'Engineering, Construction' },
+        { href: '/directory/material-handling-equipment', label: 'Material Handling Equipment' },
+        { href: '/directory/mining-equipment', label: 'Mining Equipment' },
+        { href: '/directory/other-related-equipment-products-and-services', label: 'Other Related Equipment, Products, & Services' },
+        { href: '/directory/power-and-power-transmission-equipment', label: 'Power & Power Transmission Equipment' },
+        { href: '/directory/processingpreparation-equipment', label: 'Processing/Preparation Equipment' },
+      ],
     },
     {
       label: 'Resources',
-      items: [
-      ],
+      items: [...resources],
     },
     {
       label: 'User Tools',
