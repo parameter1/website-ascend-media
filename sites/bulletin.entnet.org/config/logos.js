cb63458baeaf9d35864e4ff998119cd55329fb07
Brian Miller <brian@parameter1.com>
2021-01-14 11:43:10 -0600

    rename everthing to refrence bulletin

--- sites/bulletin.entnet.org/config/logos.js
+++ sites/bulletin.entnet.org/config/logos.js
@@ -1,11 +1,11 @@
 module.exports = {
   navbar: {
-    src: 'https://img.hub.heart.org/files/base/ascend/hearthub/image/static/isc-logo.png?h=90&auto=format,compress&q=70',
+    src: 'https://p1-cms-assets-ascend.imgix.net/files/base/ascend/bulletin/image/static/combo.png',
     srcset: [
-      'https://img.hub.heart.org/files/base/ascend/hearthub/image/static/isc-logo.png?h=180&auto=format,compress&q=70 2x',
+      'https://p1-cms-assets-ascend.imgix.net/files/base/ascend/bulletin/image/static/combo.png',
     ],
     href: '/',
-    title: 'Scientific Sessions',
+    title: 'AAO-HNSF Bulletin',
   },
   footer: {
     src: 'https://img.hub.heart.org/files/base/ascend/hearthub/image/static/footer.svg?h=90&auto=format,compress&q=70',
--- sites/bulletin.entnet.org/config/navigation.js
+++ sites/bulletin.entnet.org/config/navigation.js
@@ -6,23 +6,22 @@ const searchItem = {
 };
 
 const userTools = [
-  { href: '/page/privacy-policy', label: 'Privacy Policy' },
-  { href: '/page/terms-conditions', label: 'Terms & Conditions' },
+  // { href: '/page/privacy-policy', label: 'Privacy Policy' },
+  // { href: '/page/terms-conditions', label: 'Terms & Conditions' },
 ];
 
 const topics = [
-  { href: '/late-breaking-science', label: 'Late-Breaking Science' },
-  { href: '/isc-news', label: 'ISC News' },
-  { href: '/exhibit-hall', label: 'Exhibit Hall' },
-  { href: '#', label: 'Program' },
-  { href: '#', label: 'Claim CME', target: '_blank' },
-  { href: '#', label: 'OnDemand' },
-  { href: '/previews', label: 'Previews' },
+  { href: 'http://www.entnet.org/', label: 'www.entnet.org', target: '_blank' },
+  { href: '/', label: 'Current Issue' },
+  { href: '/magazine/53c95f861784f8066eb2c882', label: 'Archives' },
+  { href: 'http://www.entnet.org/annual_meeting', label: 'Annual Meeting', target: '_blank' },
+  { href: 'https://ratecards.ascendeventmedia.com/aao-hnsbulletin2021/', label: 'Advertise with the Bulletin', target: '_blank' },
+  { href: 'http://entconnect.entnet.org/home', label: 'ENTConnect' },
 ];
 
 const secondary = [
-  { href: 'https://AHAsessions.heart.org', label: 'Virtual Experience Access', target: '_blank' },
-  { href: 'https://aha.apprisor.org/epsWelcome.cfm?CFID=7614838&CFTOKEN=f427dce13daa111b-374EED08-CF40-A033-D6066C65AA7B755D', label: 'Posters Access', target: '_blank' },
+  // { href: 'https://AHAsessions.heart.org', label: 'Virtual Experience Access', target: '_blank' },
+  // { href: 'https://aha.apprisor.org/epsWelcome.cfm?CFID=7614838&CFTOKEN=f427dce13daa111b-374EED08-CF40-A033-D6066C65AA7B755D', label: 'Posters Access', target: '_blank' },
 ];
 
 module.exports = {
