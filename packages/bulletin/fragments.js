afe5b7203d0bc5e15f2bfe7292a1c9619ba12025
Brian Miller <brian@parameter1.com>
2021-01-18 16:36:49 -0600

    rename fragment

--- packages/bulletin/fragments.js
+++ packages/bulletin/fragments.js
@@ -1,9 +1,9 @@
 const bullitenContentList = require('./graphql/fragments/content-list');
 const bullitenContentLatest = require('./graphql/fragments/content-latest');
-const bullitenMagazineIssueArchive = require('./graphql/fragments/magazine-issue-archive');
+const magazineIssueArchive = require('./graphql/fragments/magazine-issue-archive');
 
 module.exports = {
   bullitenContentList,
   bullitenContentLatest,
-  bullitenMagazineIssueArchive,
+  magazineIssueArchive,
 };
