49fa6da7d04c4ca838c1e55a389c4ef6a1651904
Brian Miller <brian@parameter1.com>
2021-01-18 16:37:59 -0600

    update header and css

--- sites/bulletin.entnet.org/config/logos.js
+++ sites/bulletin.entnet.org/config/logos.js
@@ -1,11 +1,19 @@
 module.exports = {
   navbar: {
-    src: 'https://p1-cms-assets-ascend.imgix.net/files/base/ascend/bulletin/image/static/combo.png',
+    src: 'https://p1-cms-assets-ascend.imgix.net/files/base/ascend/bulletin/image/static/bulletin.png',
     srcset: [
-      'https://p1-cms-assets-ascend.imgix.net/files/base/ascend/bulletin/image/static/combo.png',
+      'https://p1-cms-assets-ascend.imgix.net/files/base/ascend/bulletin/image/static/bulletin.png',
     ],
     href: '/',
-    title: 'AAO-HNSF Bulletin',
+    title: 'Bulletin',
+  },
+  association: {
+    src: 'https://p1-cms-assets-ascend.imgix.net/files/base/ascend/bulletin/image/static/aaoo_logo.png',
+    srcset: [
+      'https://p1-cms-assets-ascend.imgix.net/files/base/ascend/bulletin/image/static/aaoo_logo.png',
+    ],
+    href: '/',
+    title: 'AAO-HNSF',
   },
   footer: {
     src: 'https://img.hub.heart.org/files/base/ascend/hearthub/image/static/footer.svg?h=90&auto=format,compress&q=70',
