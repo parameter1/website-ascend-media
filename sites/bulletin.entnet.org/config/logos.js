72867725bc30574fd00c5f7820e9bab4e77ebf7a
Brian Miller <brian@parameter1.com>
2021-01-19 16:40:13 -0600

    update footer logo

--- sites/bulletin.entnet.org/config/logos.js
+++ sites/bulletin.entnet.org/config/logos.js
@@ -16,9 +16,10 @@ module.exports = {
     title: 'AAO-HNSF',
   },
   footer: {
-    src: 'https://img.hub.heart.org/files/base/ascend/hearthub/image/static/footer.svg?h=90&auto=format,compress&q=70',
+    src: 'https://p1-cms-assets-ascend.imgix.net/files/base/ascend/bulletin/image/static/bulletin.png',
     srcset: [
-      'https://img.hub.heart.org/files/base/ascend/hearthub/image/static/footer.svg?h=180&auto=format,compress&q=70 2x',
+      'https://p1-cms-assets-ascend.imgix.net/files/base/ascend/bulletin/image/static/bulletin.png',
     ],
+    href: '/',
   },
 };
