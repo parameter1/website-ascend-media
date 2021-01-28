9bdc6db2675a6ddfc86e608027ac080feaadeb99
Brian Miller <brian@parameter1.com>
2021-01-28 12:52:10 -0600

    split content type display into 2 templates and routes

--- sites/bulletin.entnet.org/server/routes/content.js
+++ sites/bulletin.entnet.org/server/routes/content.js
@@ -1,8 +1,13 @@
 const { withContent } = require('@parameter1/base-cms-marko-web/middleware');
 const queryFragment = require('@ascend-media/package-shared/graphql/fragments/content-page');
+const contact = require('@ascend-media/package-bulletin/templates/content/contact');
 const content = require('@ascend-media/package-bulletin/templates/content');
 
 module.exports = (app) => {
+  app.get('/*?contact/:id(\\d{8})*', withContent({
+    template: contact,
+    queryFragment,
+  }));
   app.get('/*?:id(\\d{8})*', withContent({
     template: content,
     queryFragment,
