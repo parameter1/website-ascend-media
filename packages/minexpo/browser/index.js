80cd13f9ff510adccaed75a117f4c748f24406c6
Brian Miller <brian@parameter1.com>
2021-02-08 13:54:54 -0600

    add vue component for page input and go link

--- packages/minexpo/browser/index.js
+++ packages/minexpo/browser/index.js
@@ -1,7 +1,9 @@
 import Directory from '@ascend-media/package-directory/browser';
 import Inquiry from '@parameter1/base-cms-marko-web-inquiry/browser';
+import paginationPageNumberInput from './pagination-page-number-input.vue';
 
 export default (Browser) => {
   Directory(Browser);
   Inquiry(Browser);
+  Browser.registerComponent('PaginationPageNumberInput', paginationPageNumberInput);
 };
