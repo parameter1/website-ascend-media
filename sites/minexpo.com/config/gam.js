9f5cab292540a582da3c1b27968d8b5b61fa7fea
Brian Miller <brian@parameter1.com>
2021-02-10 14:31:14 -0600

    update leaderboard sizes

--- sites/minexpo.com/config/gam.js
+++ sites/minexpo.com/config/gam.js
@@ -4,10 +4,9 @@ const config = new GAMConfiguration('6407152', { basePath: 'minexpo' });
 
 config
   .setTemplate('leaderboard', {
-    size: [[970, 250], [970, 90], [970, 66], [728, 90], [320, 50], [300, 50], [300, 100]],
+    size: [[970, 90], [320, 50], [300, 50], [300, 100]],
     sizeMapping: [
-      { viewport: [980, 0], size: [[970, 250], [970, 90], [970, 66], [728, 90]] },
-      { viewport: [750, 0], size: [728, 90] },
+      { viewport: [980, 0], size: [[970, 90]] },
       { viewport: [320, 0], size: [[300, 50], [320, 50], [300, 100]] },
     ],
   });
