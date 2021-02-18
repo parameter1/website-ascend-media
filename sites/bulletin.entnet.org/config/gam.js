b71848e8c9b78aa37a74e8a5be41689e662d0153
Brian Miller <brian@parameter1.com>
2021-02-18 08:41:01 -0600

    Add Leaderboard 2/3 & Rail 2 ads
    Leader board 2/3 are 600x100 leaderboards

--- sites/bulletin.entnet.org/config/gam.js
+++ sites/bulletin.entnet.org/config/gam.js
@@ -4,9 +4,17 @@ const config = new GAMConfiguration('6407152', { basePath: 'bulletin' });
 
 config
   .setTemplate('leaderboard', {
-    size: [[970, 90], [320, 50], [300, 50], [300, 100]],
+    size: [[728, 90], [600, 100], [320, 50], [300, 50], [300, 100]],
     sizeMapping: [
-      { viewport: [980, 0], size: [[970, 90]] },
+      { viewport: [750, 0], size: [[728, 90]] },
+      { viewport: [620, 0], size: [[600, 100]] },
+      { viewport: [320, 0], size: [[300, 50], [320, 50], [300, 100]] },
+    ],
+  })
+  .setTemplate('leaderboard600x100', {
+    size: [[600, 100], [320, 50], [300, 50], [300, 100]],
+    sizeMapping: [
+      { viewport: [620, 0], size: [[600, 100]] },
       { viewport: [320, 0], size: [[300, 50], [320, 50], [300, 100]] },
     ],
   });
@@ -14,7 +22,10 @@ config
 config
   .setAliasAdUnits('default', [
     { name: 'lb1', templateName: 'leaderboard', path: 'lb1' },
+    { name: 'lb2', templateName: 'leaderboard600x100', path: 'lb2' },
+    { name: 'lb3', templateName: 'leaderboard600x100', path: 'lb3' },
     { name: 'rail1', options: { size: [300, 250] }, path: 'rail1' },
+    { name: 'rail2', options: { size: [300, 250] }, path: 'rail2' },
   ]);
 
 module.exports = config;
