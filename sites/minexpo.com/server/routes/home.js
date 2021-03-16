e642080d47ad8a561ff717f122d4598ef5bf28d7
Brian Miller <brian@parameter1.com>
2021-03-16 10:33:52 -0500

    update homepage to use directory layout
    adjust route to use directory page on homepage and set page alias to directory for now.  Will bring back home page later

--- sites/minexpo.com/server/routes/home.js
+++ sites/minexpo.com/server/routes/home.js
@@ -1,12 +1,13 @@
 const { withWebsiteSection } = require('@parameter1/base-cms-marko-web/middleware');
 // const home = require('../templates/index');
-const home = require('../templates/index');
+// const home = require('../templates/index');
+const directory = require('@ascend-media/package-minexpo/templates/website-section/directory');
 const queryFragment = require('../graphql/fragments/website-section-page');
 
 module.exports = (app) => {
   app.get('/', withWebsiteSection({
     aliasResolver: () => 'home',
-    template: home,
+    template: directory,
     queryFragment,
   }));
 };
