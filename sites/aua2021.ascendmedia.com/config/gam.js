017d858b3930174249a5070008111584125a7cf0
Brian Miller <brian@parameter1.com>
2021-04-16 12:00:57 -0500

    add gam

--- sites/aua2021.ascendmedia.com/config/gam.js
+++ sites/aua2021.ascendmedia.com/config/gam.js
@@ -1,6 +1,6 @@
 const configureGAM = require('@ascend-media/package-global/config/gam');
 
-const config = configureGAM({ basePath: 'isc' });
+const config = configureGAM({ basePath: 'aua' });
 
 config
   .setAliasAdUnits('default', [
