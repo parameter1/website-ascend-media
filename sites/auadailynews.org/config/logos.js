081693005b0808e36f840a73d0a885e99669b6b0
Brian Miller <brian@parameter1.com>
2021-05-07 08:30:13 -0500

    fix double res call

--- sites/auadailynews.org/config/logos.js
+++ sites/auadailynews.org/config/logos.js
@@ -2,7 +2,7 @@ module.exports = {
   navbar: {
     src: 'https://img.ascendmedia.com/files/base/ascend/hh/image/static/aua/site_logo.png?h=100',
     srcset: [
-      'https://img.ascendmedia.com/files/base/ascend/hh/image/static/aua/site_logo.png?h=100 2x',
+      'https://img.ascendmedia.com/files/base/ascend/hh/image/static/aua/site_logo.png?h=200 2x',
     ],
     href: '/',
     title: 'AUA Daily News',
@@ -10,7 +10,7 @@ module.exports = {
   footer: {
     src: 'https://img.ascendmedia.com/files/base/ascend/hh/image/static/aua/site_logo.png?h=100',
     srcset: [
-      'https://img.ascendmedia.com/files/base/ascend/hh/image/static/aua/site_logo.png?h=100 2x',
+      'https://img.ascendmedia.com/files/base/ascend/hh/image/static/aua/site_logo.png?h=200 2x',
     ],
   },
 };
