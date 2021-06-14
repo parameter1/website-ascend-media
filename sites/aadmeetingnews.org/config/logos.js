3693f0c4fdb9c78bc9b8863855749b163adae373
Shinsina <shinsina@att.net>
2021-06-14 13:38:52 -0500

    Update Logo Sizing

--- sites/aadmeetingnews.org/config/logos.js
+++ sites/aadmeetingnews.org/config/logos.js
@@ -1,6 +1,6 @@
 module.exports = {
   navbar: {
-    src: 'https://img.ascendmedia.com/files/base/ascend/hh/image/static/aad/AAD_Logo_Header.png?h=100',
+    src: 'https://img.ascendmedia.com/files/base/ascend/hh/image/static/aad/AAD_Logo_Header.png?h=200',
     srcset: [
       'https://img.ascendmedia.com/files/base/ascend/hh/image/static/aad/AAD_Logo_Header.png?h=200 2x',
     ],
@@ -8,7 +8,7 @@ module.exports = {
     title: 'AAD Meeting News',
   },
   footer: {
-    src: 'https://img.ascendmedia.com/files/base/ascend/hh/image/static/aad/AAD_Logo_Footer.png?h=100',
+    src: 'https://img.ascendmedia.com/files/base/ascend/hh/image/static/aad/AAD_Logo_Footer.png?h=200',
     srcset: [
       'https://img.ascendmedia.com/files/base/ascend/hh/image/static/aad/AAD_Logo_Footer.png?h=200 2x',
     ],
