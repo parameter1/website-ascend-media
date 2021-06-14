e03592ac9c2654fa8868f10aa7c73a418f2d8bbc
Shinsina <shinsina@att.net>
2021-06-14 12:59:26 -0500

    Logo Update

--- sites/aadmeetingnews.org/config/logos.js
+++ sites/aadmeetingnews.org/config/logos.js
@@ -1,16 +1,16 @@
 module.exports = {
   navbar: {
-    src: 'https://img.ascendmedia.com/files/base/ascend/hh/image/static/aua/site_logo.png?h=100',
+    src: 'https://img.ascendmedia.com/files/base/ascend/hh/image/static/aad/AAD_Logo_Header.png?h=100',
     srcset: [
-      'https://img.ascendmedia.com/files/base/ascend/hh/image/static/aua/site_logo.png?h=200 2x',
+      'https://img.ascendmedia.com/files/base/ascend/hh/image/static/aad/AAD_Logo_Header.png?h=200 2x',
     ],
     href: '/',
-    title: 'AUA Daily News',
+    title: 'aad Daily News',
   },
   footer: {
-    src: 'https://img.ascendmedia.com/files/base/ascend/hh/image/static/aua/site_logo.png?h=100',
+    src: 'https://img.ascendmedia.com/files/base/ascend/hh/image/static/aad/AAD_Logo_Footer.png?h=100',
     srcset: [
-      'https://img.ascendmedia.com/files/base/ascend/hh/image/static/aua/site_logo.png?h=200 2x',
+      'https://img.ascendmedia.com/files/base/ascend/hh/image/static/aad/AAD_Logo_Footer.png?h=200 2x',
     ],
   },
 };
